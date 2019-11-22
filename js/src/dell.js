872ed2748e1c0da815c6522d3a3fa393db176e70
goasb <goasb18@gmail.com>
2019-11-22 18:32:34 +0900

    20191122

--- js/src/dell.js
+++ js/src/dell.js
@@ -1,5 +1,28 @@
 // dell.js
 
 (function($){
+   const conBox = $('#contentsBox');
+   const slideUl = conBox.find('ul');
+   const slideLi = slideUl.children('li');
+   const slideA = slideLi.children('.indi'); //인디케이터
+      console.log(slideA);
+   const imgList = conBox.find('.slide_img_list');
+   let imgSlide = imgList.find('.slide');
+
+   imgSlide.eq(-1).clone().prependTo('.slide_img_list');
+   imgSlide = imgList.find('.slide');
+
+   // ------------------------------------
+   i=0;
+   imgLen = imgSlide.length;
+
+   
+
+   slideA.on('click',function(){
+      let index = $(this).parent('li').index();
+      imgList.animate({marginLeft:-100 * index + "%" });
+      console.log(index);
+   });
+
 
 })(jQuery);
